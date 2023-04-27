3e6f4bef616ec38758f5fcf0df61f14205e9a248
JOdiemo <odiemo@gmail.com>
2023-04-28 00:58:36 +0300

    comiting the likes functionality

---
+++ src/modules/likes.js
@@ -0,0 +1,23 @@
+const involveURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi';
+
+const postLike = async (id) => {
+  const response = await fetch(
+    `${involveURL}/apps/AEzJkDXLf7FGXWlpS77t/likes`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        item_id: id,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    },
+  );
+};
+
+const getLikes = async () => {
+  const response = await fetch(
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AEzJkDXLf7FGXWlpS77t/likes',
+  );
+  const numOfLikes = await response.json();
+  return numOfLikes;
+};
+export { postLike, getLikes };
